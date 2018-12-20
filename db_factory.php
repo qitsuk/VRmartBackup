7c191762293b2bf2e7553159b27c245b9e845008
qitsuk <kasper.andreasen@gmail.com>
2018-12-20 10:23:47 +0100

    Added basic functionality to the get_products file

--- db_factory.php
+++ db_factory.php
@@ -1,2 +1,6 @@
 <?php
 $dbc = mysqli_connect("mysql79.unoeuro.com", "qitsuk_dk", "19qItSuKsUnOEuRo87", "qitsuk_dk_db2", '3306');
+if ($dbc->connect_errno) {
+    printf("Connection failed: %s\n", $dbc->connect_error);
+    exit();
+}
--- get_product.php
+++ get_product.php
@@ -1,2 +1,17 @@
 <?php
 include_once 'db_factory.php';
+
+if (isset($_GET['data'])) {
+    $productID = $_GET['data'];
+    $q1 = "SELECT * FROM Produkter WHERE p_id=$productID";
+} else {
+    echo "Error, please try again.";
+}
+
+$result = $dbc->query($q1);
+$resultArray = array();
+while ($row = $result->fetch_assoc()) {
+    array_push($resultArray, $row);
+}
+
+echo json_encode($resultArray);
